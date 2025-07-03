976aa215518b61c42435ebc46a1ae10277cedd68
Chris Ray <cray4427@ksu.edu>
2025-07-03 14:39:26 -0500

    api changes

--- client/src/api/blogPosts.ts
+++ client/src/api/blogPosts.ts
@@ -1,13 +1,13 @@
 import { BlogPost } from "@/types/types";
 
 export async function getAllPosts(): Promise<BlogPost[]> {
-  const res = await fetch("/api/posts");
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/api/posts`);
   if (!res.ok) throw new Error("Failed to fetch posts");
   return res.json();
 }
 
 export async function getPostById(id: number): Promise<BlogPost> {
-  const res = await fetch(`/api/posts/${id}`);
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/api/posts/${id}`);
   if (!res.ok) {
     if (res.status === 404) throw new Error("Post not found");
     throw new Error("Failed to fetch post");
--- client/src/api/projects.ts
+++ client/src/api/projects.ts
@@ -1,13 +1,13 @@
 import { Project } from "@/types/types";
 
 export async function getAllProjects(): Promise<Project[]> {
-  const res = await fetch("/api/projects");
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/api/projects`);
   if (!res.ok) throw new Error("Failed to fetch posts");
   return res.json();
 }
 
 export async function getProjectById(id: number): Promise<Project> {
-  const res = await fetch(`/api/projects/${id}`);
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/api/projects/${id}`);
   if (!res.ok) {
     if (res.status === 404) throw new Error("Post not found");
     throw new Error("Failed to fetch post");
